27065745301899aa8d8d30b874aa48700b4c59cf
Ignacy Stępka <ignacy.stepka@gmail.com>
2024-04-02 21:30:30 +0200

    Move data to separate folder

--- parse.py
+++ parse.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
-criteria = pd.read_csv('criteria.csv')
-data = pd.read_csv('data.csv')
-decision_classes = pd.read_csv('decision_classes.csv')
+criteria = pd.read_csv('data/criteria.csv')
+data = pd.read_csv('data/data.csv')
+decision_classes = pd.read_csv('data/decision_classes.csv')
+pairwise_comparisons = pd.read_csv('data/pairwise_comp.csv')
 
 if __name__ == '__main__':
     print(criteria)
     print(data)
     print(decision_classes)
+    print(pairwise_comparisons)
