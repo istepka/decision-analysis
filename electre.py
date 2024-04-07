d406bdf1f5368aed9ea35d237c49208f4a6820fb
Michal Wilinski <m.wilinski11@gmail.com>
2024-04-08 01:03:22 +0200

    chore: black formatter pass

--- electre.py
+++ electre.py
@@ -4,39 +4,63 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-def concordance_function(difference,indifference,preference,gain):
+def concordance_function(difference, indifference, preference, gain):
     if gain:
         if difference >= -indifference:
             return 1
         elif difference < -preference:
             return 0
         else:
-            return (preference + difference) / (preference-indifference)
+            return (preference + difference) / (preference - indifference)
     else:
         if difference <= indifference:
             return 1
         elif difference > preference:
             return 0
         else:
-            return (preference-difference) / (preference-indifference)
+            return (preference - difference) / (preference - indifference)
 
 
-def calculate_marginal_concordance(alternatives, boundary_profiles, indifference_thresholds, preference_thresholds, gain,concordance_function=concordance_function):
+def marginal_concordance(
+    alternatives,
+    boundary_profiles,
+    indifference_thresholds,
+    preference_thresholds,
+    gain,
+    concordance_function=concordance_function,
+):
 
-    marginal_concordance_alt_to_profile = np.zeros((len(boundary_profiles),len(alternatives),len(alternatives.columns),))
-    marginal_concordance_profile_to_alt = np.zeros((len(boundary_profiles),len(alternatives),len(alternatives.columns),))
+    marginal_concordance_alt_to_profile = np.zeros(
+        (
+            len(boundary_profiles),
+            len(alternatives),
+            len(alternatives.columns),
+        )
+    )
+    marginal_concordance_profile_to_alt = np.zeros(
+        (
+            len(boundary_profiles),
+            len(alternatives),
+            len(alternatives.columns),
+        )
+    )
 
     for i in range(len(alternatives)):
         for j in range(len(boundary_profiles)):
             for k in range(len(alternatives.columns)):
-                difference = alternatives.iloc[i,k] - boundary_profiles.iloc[j,k]
-                
+                difference = alternatives.iloc[i, k] - boundary_profiles.iloc[j, k]
+
                 indifference = indifference_thresholds[j][k]
                 preference = preference_thresholds[j][k]
-                marginal_concordance_alt_to_profile[j,i,k] = concordance_function(difference, indifference, preference, gain[k])
-                marginal_concordance_profile_to_alt[j,i,k] = concordance_function(-difference, indifference, preference, gain[k])
+                marginal_concordance_alt_to_profile[j, i, k] = concordance_function(
+                    difference, indifference, preference, gain[k]
+                )
+                marginal_concordance_profile_to_alt[j, i, k] = concordance_function(
+                    -difference, indifference, preference, gain[k]
+                )
 
-    return marginal_concordance_alt_to_profile,marginal_concordance_profile_to_alt
+    return marginal_concordance_alt_to_profile, marginal_concordance_profile_to_alt
+
 
 def discordance_function(difference, veto, preference, gain):
     if gain:
@@ -44,121 +68,218 @@ def discordance_function(difference, veto, preference, gain):
             return 1
         elif difference >= -preference:
             return 0
-        else :
-            return (-difference - preference) / (veto-preference)
+        else:
+            return (-difference - preference) / (veto - preference)
     else:
         if difference >= veto:
             return 1
         elif difference <= preference:
             return 0
         else:
-            return (veto - difference) / (veto-preference)
+            return (veto - difference) / (veto - preference)
 
-def calculate_marginal_discordance(alternatives,boundary_profiles,preference_thresholds,veto_thresholds,gain,discordance_function=discordance_function):
 
-    marginal_discordance_alt_to_profile = np.zeros((len(boundary_profiles),len(alternatives),len(alternatives.columns),))
-    marginal_discordance_profile_to_alt = np.zeros((len(boundary_profiles),len(alternatives),len(alternatives.columns),))
+def marginal_discordance(
+    alternatives,
+    boundary_profiles,
+    preference_thresholds,
+    veto_thresholds,
+    gain,
+    discordance_function=discordance_function,
+):
+
+    marginal_discordance_alt_to_profile = np.zeros(
+        (
+            len(boundary_profiles),
+            len(alternatives),
+            len(alternatives.columns),
+        )
+    )
+    marginal_discordance_profile_to_alt = np.zeros(
+        (
+            len(boundary_profiles),
+            len(alternatives),
+            len(alternatives.columns),
+        )
+    )
 
     for i in range(len(alternatives)):
         for j in range(len(boundary_profiles)):
             for k in range(len(alternatives.columns)):
-                difference = alternatives.iloc[i,k] - boundary_profiles.iloc[j,k]
-                
+                difference = alternatives.iloc[i, k] - boundary_profiles.iloc[j, k]
+
                 veto = veto_thresholds[j][k]
                 preference = preference_thresholds[j][k]
-                marginal_discordance_alt_to_profile[j,i,k] = discordance_function(difference, veto, preference, gain[k])
-                marginal_discordance_profile_to_alt[j,i,k] = discordance_function(-difference, veto, preference, gain[k])
+                marginal_discordance_alt_to_profile[j, i, k] = discordance_function(
+                    difference, veto, preference, gain[k]
+                )
+                marginal_discordance_profile_to_alt[j, i, k] = discordance_function(
+                    -difference, veto, preference, gain[k]
+                )
 
-    return marginal_discordance_alt_to_profile,marginal_discordance_profile_to_alt
+    return marginal_discordance_alt_to_profile, marginal_discordance_profile_to_alt
 
-def calculate_comprehensive_concordance(concordance_matrix_alt_to_profile, concordance_matrix_profile_to_alt,weights):
 
-    comprehensive_concordance_alt_to_profile = np.average(concordance_matrix_alt_to_profile, axis=2, weights=weights)
-    comprehensive_concordance_profile_to_alt = np.average(concordance_matrix_profile_to_alt, axis=2, weights=weights)
+def comprehensive_concordance(
+    concordance_matrix_alt_to_profile, concordance_matrix_profile_to_alt, weights
+):
 
-    return comprehensive_concordance_alt_to_profile.T, comprehensive_concordance_profile_to_alt.T
+    comprehensive_concordance_alt_to_profile = np.average(
+        concordance_matrix_alt_to_profile, axis=2, weights=weights
+    )
+    comprehensive_concordance_profile_to_alt = np.average(
+        concordance_matrix_profile_to_alt, axis=2, weights=weights
+    )
 
-def calculate_outranking_credibility(comprehensive_concordance,marginal_discordance):
-    outrankings = np.zeros((len(comprehensive_concordance[0]),len(comprehensive_concordance[0][0]),2))
-    for i in range(len(comprehensive_concordance[0])): # numver of alternatives
-        for j in range(len(comprehensive_concordance[0][0])): # number of boundary profiles
-            outranking = [comprehensive_concordance[0][i][j], comprehensive_concordance[1][i][j]]
-            for k in range(len(marginal_discordance[0])): # number of criteria
-                
-                if comprehensive_concordance[0][i][j] < marginal_discordance[0][j][i][k]: # alt_to_profile
-                    outranking[0] *= (1-marginal_discordance[0][j][i][k])/(1-comprehensive_concordance[0][i][j])
-                if comprehensive_concordance[1][i][j] < marginal_discordance[1][j][i][k]: # profile_to_alt
-                    outranking[1] *= (1-marginal_discordance[1][j][i][k])/(1-comprehensive_concordance[1][i][j])
+    return (
+        comprehensive_concordance_alt_to_profile.T,
+        comprehensive_concordance_profile_to_alt.T,
+    )
+
+
+def outranking_credibility(comprehensive_concordance, marginal_discordance):
+    outrankings = np.zeros(
+        (len(comprehensive_concordance[0]), len(comprehensive_concordance[0][0]), 2)
+    )
+    for i in range(len(comprehensive_concordance[0])):
+        for j in range(
+            len(comprehensive_concordance[0][0])
+        ): 
+            outranking = [
+                comprehensive_concordance[0][i][j],
+                comprehensive_concordance[1][i][j],
+            ]
+            for k in range(len(marginal_discordance[0])):
+
+                if (
+                    comprehensive_concordance[0][i][j]
+                    < marginal_discordance[0][j][i][k]
+                ):
+                    outranking[0] *= (1 - marginal_discordance[0][j][i][k]) / (
+                        1 - comprehensive_concordance[0][i][j]
+                    )
+                if (
+                    comprehensive_concordance[1][i][j]
+                    < marginal_discordance[1][j][i][k]
+                ):
+                    outranking[1] *= (1 - marginal_discordance[1][j][i][k]) / (
+                        1 - comprehensive_concordance[1][i][j]
+                    )
             outrankings[i][j] = outranking
-    
+
     return outrankings
 
-def preference_aggregation(aPb,bPa):
+
+def preference_aggregation(aPb, bPa):
     if aPb and not bPa:
         return 1
     if not aPb and bPa:
         return -1
-    if aPb and bPa: 
+    if aPb and bPa:
         return 0
     if not aPb and not bPa:
         return None
 
-def transform_outranking_to_preference(outrankings,credibility_threshold=0.65):
 
-    outranking_preference = np.zeros((len(outrankings),len(outrankings[0])))
+def outranking_to_preference(outrankings, credibility_threshold=0.65):
+
+    outranking_preference = np.zeros((len(outrankings), len(outrankings[0])))
     for i in range(len(outrankings)):
         for j in range(len(outrankings[0])):
-            outranking_preference[i][j] = preference_aggregation(outrankings[i][j][0] >= credibility_threshold, outrankings[i][j][1] >= credibility_threshold)
+            outranking_preference[i][j] = preference_aggregation(
+                outrankings[i][j][0] >= credibility_threshold,
+                outrankings[i][j][1] >= credibility_threshold,
+            )
     return outranking_preference
 
-def class_assignment(preference_matrix,num_classes, pessimistic=True):
+
+def class_assignment(preference_matrix, num_classes, pessimistic=True):
     if pessimistic:
-        classes = np.full(len(preference_matrix),num_classes)
+        classes = np.full(len(preference_matrix), num_classes)
 
     else:
         classes = np.ones(len(preference_matrix))
 
-
-    for j,a in enumerate(preference_matrix):
+    for j, a in enumerate(preference_matrix):
         if pessimistic:
-            for i in range(len(a)-1,-1,-1):
-                
-                if a[i] >=0:
+            for i in range(len(a) - 1, -1, -1):
+
+                if a[i] >= 0:
                     break
-                classes[j]-=1
-            
+                classes[j] -= 1
+
         else:
             for i in range(len(a)):
                 if a[i] == -1:
                     break
-                classes[j]+=1
-                
-    return classes 
- 
-def electre_tri_b(alternatives,boundary_profiles,criteria_is_gain,indifference_thresholds,preference_thresholds,veto_thresholds,weights,pessimistic=True):
-
-    concordance_matrix_alt_to_profile, concordance_matrix_profile_to_alt = calculate_marginal_concordance(alternatives,boundary_profiles,indifference_thresholds,preference_thresholds,criteria_is_gain)
-    discordance_matrix_alt_to_profile, discordance_matrix_profile_to_alt = calculate_marginal_discordance(alternatives,boundary_profiles,preference_thresholds,veto_thresholds,criteria_is_gain)
-
-    comprehensive_concordance = calculate_comprehensive_concordance(concordance_matrix_alt_to_profile,concordance_matrix_profile_to_alt,weights)
-
-    outrankings = calculate_outranking_credibility(comprehensive_concordance,[discordance_matrix_alt_to_profile,discordance_matrix_profile_to_alt])
-
-    outranking_preference = transform_outranking_to_preference(outrankings)
-
-    classes = class_assignment(outranking_preference,len(boundary_profiles)+1,pessimistic)
+                classes[j] += 1
 
     return classes
 
+
+def electre_tri_b(
+    alternatives,
+    boundary_profiles,
+    is_gain_criterion,
+    indifference_thresholds,
+    preference_thresholds,
+    veto_thresholds,
+    weights,
+    pessimistic=True,
+):
+
+    concordance_matrix_alt_to_profile, concordance_matrix_profile_to_alt = (
+        marginal_concordance(
+            alternatives,
+            boundary_profiles,
+            indifference_thresholds,
+            preference_thresholds,
+            is_gain_criterion,
+        )
+    )
+    discordance_matrix_alt_to_profile, discordance_matrix_profile_to_alt = (
+        marginal_discordance(
+            alternatives,
+            boundary_profiles,
+            preference_thresholds,
+            veto_thresholds,
+            is_gain_criterion,
+        )
+    )
+
+    comprehensive_concordance_matrix = comprehensive_concordance(
+        concordance_matrix_alt_to_profile, concordance_matrix_profile_to_alt, weights
+    )
+
+    outrankings = outranking_credibility(
+        comprehensive_concordance_matrix,
+        [discordance_matrix_alt_to_profile, discordance_matrix_profile_to_alt],
+    )
+
+    outranking_preference = outranking_to_preference(outrankings)
+
+    classes = class_assignment(
+        outranking_preference, len(boundary_profiles) + 1, pessimistic
+    )
+
+    return classes
+
+
 def plot_electre_classes(data, class_column):
     G = nx.DiGraph()
-    for name, level in zip(data['Movie'], data[class_column]):
+    for name, level in zip(data["Movie"], data[class_column]):
         G.add_node(name, level=level)
-        print(int(level), end=' ')
+        print(int(level), end=" ")
     print()
 
-    pos = nx.multipartite_layout(G, subset_key="level", align='horizontal')
-    nx.draw(G, pos, with_labels=True, node_size=1000, node_color='skyblue', font_size=7, font_weight='bold')    
+    pos = nx.multipartite_layout(G, subset_key="level", align="horizontal")
+    nx.draw(
+        G,
+        pos,
+        with_labels=True,
+        node_size=1000,
+        node_color="skyblue",
+        font_size=7,
+        font_weight="bold",
+    )
     plt.show()
-
- 
--- electre_tests.py
+++ electre_tests.py
@@ -2,151 +2,228 @@ import numpy as np
 import pandas as pd
 from electre import *
 
+
 def test_marginal_concordance():
-    test_alternatives = pd.DataFrame([[90,86,46,30],
-    [40,90,14,48],
-    [94,100,40,36],
-    [78,76,30,50],
-    [60,60,30,30],
-    [64,72,12,46],
-    [62,88,22,48],
-    [70,30,12,12]]).astype(float)
-    test_boundary_profiles = pd.DataFrame( [[64,61,32,32],
-[86,84,43,43]]).astype(float)
-    test_indifference_thresholds = [[2,2,0,0],[3,2,0,0]]
-    test_preference_thresholds = [[6,5,2,2],[7,8,2,2]]
+    test_alternatives = pd.DataFrame(
+        [
+            [90, 86, 46, 30],
+            [40, 90, 14, 48],
+            [94, 100, 40, 36],
+            [78, 76, 30, 50],
+            [60, 60, 30, 30],
+            [64, 72, 12, 46],
+            [62, 88, 22, 48],
+            [70, 30, 12, 12],
+        ]
+    ).astype(float)
+    test_boundary_profiles = pd.DataFrame([[64, 61, 32, 32], [86, 84, 43, 43]]).astype(
+        float
+    )
+    test_indifference_thresholds = [[2, 2, 0, 0], [3, 2, 0, 0]]
+    test_preference_thresholds = [[6, 5, 2, 2], [7, 8, 2, 2]]
 
-    test_gains = [True,True,True,True]
+    test_gains = [True, True, True, True]
 
-    marginal_concordance_alt_to_profile, marginal_concordance_profile_to_alt = calculate_marginal_concordance(test_alternatives, test_boundary_profiles, test_indifference_thresholds, test_preference_thresholds, test_gains)
+    marginal_concordance_alt_to_profile, marginal_concordance_profile_to_alt = (
+        marginal_concordance(
+            test_alternatives,
+            test_boundary_profiles,
+            test_indifference_thresholds,
+            test_preference_thresholds,
+            test_gains,
+        )
+    )
 
     concordance_matrix_alt_to_profile = np.array(
         [
-        [1,1,1,0],
-        [0,1,0,1],
-        [1,1,1,1],
-        [1,1,0,1],
-        [0.5,1,0,0],
-        [1,1,0,1],
-        [1,1,0,1],
-        [1,0,0,0]
+            [1, 1, 1, 0],
+            [0, 1, 0, 1],
+            [1, 1, 1, 1],
+            [1, 1, 0, 1],
+            [0.5, 1, 0, 0],
+            [1, 1, 0, 1],
+            [1, 1, 0, 1],
+            [1, 0, 0, 0],
         ],
     )
     concordance_matrix_profile_to_alt = np.array(
         [
-        [0,0,0,1],
-        [1,0,1,0],
-        [0,0,0,0],
-        [0,0,1,0],
-        [1,1,1,1],
-        [1,0,1,0],
-        [1,0,1,0],
-        [0,1,1,1]
+            [0, 0, 0, 1],
+            [1, 0, 1, 0],
+            [0, 0, 0, 0],
+            [0, 0, 1, 0],
+            [1, 1, 1, 1],
+            [1, 0, 1, 0],
+            [1, 0, 1, 0],
+            [0, 1, 1, 1],
         ]
     )
-    assert np.allclose(marginal_concordance_alt_to_profile[0], concordance_matrix_alt_to_profile, atol=0.01)
-    assert np.allclose(marginal_concordance_profile_to_alt[0], concordance_matrix_profile_to_alt, atol=0.01)
+    assert np.allclose(
+        marginal_concordance_alt_to_profile[0],
+        concordance_matrix_alt_to_profile,
+        atol=0.01,
+    )
+    assert np.allclose(
+        marginal_concordance_profile_to_alt[0],
+        concordance_matrix_profile_to_alt,
+        atol=0.01,
+    )
 
-    return marginal_concordance_alt_to_profile, marginal_concordance_profile_to_alt  
+    return marginal_concordance_alt_to_profile, marginal_concordance_profile_to_alt
+
 
 def test_marginal_discordance():
 
-    test_alternatives = pd.DataFrame([[90,86,46,30],
-    [40,90,14,48],
-    [94,100,40,36],
-    [78,76,30,50],
-    [60,60,30,30],
-    [64,72,12,46],
-    [62,88,22,48],
-    [70,30,12,12]]).astype(float)
-    test_boundary_profiles = pd.DataFrame( [[64,61,32,32],
-[86,84,43,43]]).astype(float)
-    test_veto_thresholds = [[20,24,np.inf,np.inf],[20,25,np.inf,np.inf]]
-    test_preference_thresholds = [[6,5,2,2],[7,8,2,2]]
+    test_alternatives = pd.DataFrame(
+        [
+            [90, 86, 46, 30],
+            [40, 90, 14, 48],
+            [94, 100, 40, 36],
+            [78, 76, 30, 50],
+            [60, 60, 30, 30],
+            [64, 72, 12, 46],
+            [62, 88, 22, 48],
+            [70, 30, 12, 12],
+        ]
+    ).astype(float)
+    test_boundary_profiles = pd.DataFrame([[64, 61, 32, 32], [86, 84, 43, 43]]).astype(
+        float
+    )
+    test_veto_thresholds = [[20, 24, np.inf, np.inf], [20, 25, np.inf, np.inf]]
+    test_preference_thresholds = [[6, 5, 2, 2], [7, 8, 2, 2]]
 
-    test_gains = [True,True,True,True]
+    test_gains = [True, True, True, True]
 
-    discordance_alt_to_profile = np.array([
-        [0,0,0,0],
-        [1,0,0,0],
-        [0,0,0,0],
-        [0.07,0,0,0],
-        [1,0.94,0,0],
-        [1,0.23,0,0],
-        [1,0,0,0],
-        [0.69,1,0,0]
-    ])
-    discordance_profile_to_alt = np.array([
-        [1,1,0,0],
-        [0,1,0,0],
-        [1,1,0,0],
-        [0.57,0.52,0,0],
-        [0,0,0,0],
-        [0,0.31,0,0],
-        [0,1,0,0],
-        [0,0,0,0]
-    ])
+    discordance_alt_to_profile = np.array(
+        [
+            [0, 0, 0, 0],
+            [1, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0.07, 0, 0, 0],
+            [1, 0.94, 0, 0],
+            [1, 0.23, 0, 0],
+            [1, 0, 0, 0],
+            [0.69, 1, 0, 0],
+        ]
+    )
+    discordance_profile_to_alt = np.array(
+        [
+            [1, 1, 0, 0],
+            [0, 1, 0, 0],
+            [1, 1, 0, 0],
+            [0.57, 0.52, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0.31, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0],
+        ]
+    )
 
-    marginal_discordance_alt_to_profile, marginal_discordance_profile_to_alt = calculate_marginal_discordance(test_alternatives, test_boundary_profiles, test_preference_thresholds, test_veto_thresholds,test_gains)
+    marginal_discordance_alt_to_profile, marginal_discordance_profile_to_alt = (
+        marginal_discordance(
+            test_alternatives,
+            test_boundary_profiles,
+            test_preference_thresholds,
+            test_veto_thresholds,
+            test_gains,
+        )
+    )
 
-    assert np.allclose(marginal_discordance_alt_to_profile[1], discordance_alt_to_profile, atol=0.01)
-    assert np.allclose(marginal_discordance_profile_to_alt[0], discordance_profile_to_alt, atol=0.01)
+    assert np.allclose(
+        marginal_discordance_alt_to_profile[1], discordance_alt_to_profile, atol=0.01
+    )
+    assert np.allclose(
+        marginal_discordance_profile_to_alt[0], discordance_profile_to_alt, atol=0.01
+    )
 
     return marginal_discordance_alt_to_profile, marginal_discordance_profile_to_alt
 
+
 def test_comprehensive_concordance():
 
-    concordance_matrix_alt_to_profile, concordance_matrix_profile_to_alt = test_marginal_concordance()
+    concordance_matrix_alt_to_profile, concordance_matrix_profile_to_alt = (
+        test_marginal_concordance()
+    )
 
-    weights = [0.4,0.3,0.25,0.05]
+    weights = [0.4, 0.3, 0.25, 0.05]
 
-    comprehensive_concordance_alt_to_profile, comprehensive_concordance_profile_to_alt = calculate_comprehensive_concordance(concordance_matrix_alt_to_profile, concordance_matrix_profile_to_alt,weights)
-    return comprehensive_concordance_alt_to_profile, comprehensive_concordance_profile_to_alt
+    (
+        comprehensive_concordance_alt_to_profile,
+        comprehensive_concordance_profile_to_alt,
+    ) = comprehensive_concordance(
+        concordance_matrix_alt_to_profile, concordance_matrix_profile_to_alt, weights
+    )
+    return (
+        comprehensive_concordance_alt_to_profile,
+        comprehensive_concordance_profile_to_alt,
+    )
+
 
 def test_outranking_credibility():
 
     comprehensive_concordance = test_comprehensive_concordance()
     marginal_discordance = test_marginal_discordance()
 
-    outrankings = calculate_outranking_credibility(comprehensive_concordance,marginal_discordance)
-    test_outrankings = np.array([[0.95,0],[0.95,0.65]])
+    outrankings = outranking_credibility(
+        comprehensive_concordance, marginal_discordance
+    )
+    test_outrankings = np.array([[0.95, 0], [0.95, 0.65]])
     assert np.allclose(outrankings[0], test_outrankings, atol=0.01)
     return outrankings
 
+
 def test_transform_outranking_to_preference():
     outrankings = test_outranking_credibility()
-    outranking_preference = transform_outranking_to_preference(outrankings)
-    assert np.allclose(outranking_preference[:,0:1].flatten(), [[1,np.nan,1,1,-1,0,1,np.nan]], equal_nan=True)
+    outranking_preference = outranking_to_preference(outrankings)
+    assert np.allclose(
+        outranking_preference[:, 0:1].flatten(),
+        [[1, np.nan, 1, 1, -1, 0, 1, np.nan]],
+        equal_nan=True,
+    )
     return outranking_preference
+
 
 def test_class_assignment():
 
     preference = test_transform_outranking_to_preference()
-    classes_pess = class_assignment(preference,3,True)
-    classes_opt = class_assignment(preference,3,False)
-    assert np.allclose(classes_pess, [3,1,3,2,1,2,2,1])
-    assert np.allclose(classes_opt, [3,2,3,2,1,2,2,2])
+    classes_pess = class_assignment(preference, 3, True)
+    classes_opt = class_assignment(preference, 3, False)
+    assert np.allclose(classes_pess, [3, 1, 3, 2, 1, 2, 2, 1])
+    assert np.allclose(classes_opt, [3, 2, 3, 2, 1, 2, 2, 2])
     return classes_pess, classes_opt
 
+
 def test_electre_tri_b():
-    
-    test_alternatives = pd.DataFrame([[90,86,46,30],
-    [40,90,14,48],
-    [94,100,40,36],
-    [78,76,30,50],
-    [60,60,30,30],
-    [64,72,12,46],
-    [62,88,22,48],
-    [70,30,12,12]]).astype(float)
-    test_boundary_profiles = pd.DataFrame( [[64,61,32,32],
-[86,84,43,43]]).astype(float)
-    test_veto_thresholds = [[20,24,np.inf,np.inf],[20,25,np.inf,np.inf]]
-    test_preference_thresholds = [[6,5,2,2],[7,8,2,2]]
-    test_indifference_thresholds = [[2,2,0,0],[3,2,0,0]]
-    test_gains = [True,True,True,True]
-    test_weights = [0.4,0.3,0.25,0.05]
 
-    classes = electre_tri_b(test_alternatives,test_boundary_profiles,test_gains,test_indifference_thresholds,test_preference_thresholds,test_veto_thresholds,test_weights)
-    assert np.allclose(classes, [3,1,3,2,1,2,2,1])
+    test_alternatives = pd.DataFrame(
+        [
+            [90, 86, 46, 30],
+            [40, 90, 14, 48],
+            [94, 100, 40, 36],
+            [78, 76, 30, 50],
+            [60, 60, 30, 30],
+            [64, 72, 12, 46],
+            [62, 88, 22, 48],
+            [70, 30, 12, 12],
+        ]
+    ).astype(float)
+    test_boundary_profiles = pd.DataFrame([[64, 61, 32, 32], [86, 84, 43, 43]]).astype(
+        float
+    )
+    test_veto_thresholds = [[20, 24, np.inf, np.inf], [20, 25, np.inf, np.inf]]
+    test_preference_thresholds = [[6, 5, 2, 2], [7, 8, 2, 2]]
+    test_indifference_thresholds = [[2, 2, 0, 0], [3, 2, 0, 0]]
+    test_gains = [True, True, True, True]
+    test_weights = [0.4, 0.3, 0.25, 0.05]
+
+    classes = electre_tri_b(
+        test_alternatives,
+        test_boundary_profiles,
+        test_gains,
+        test_indifference_thresholds,
+        test_preference_thresholds,
+        test_veto_thresholds,
+        test_weights,
+    )
+    assert np.allclose(classes, [3, 1, 3, 2, 1, 2, 2, 1])
     return classes
-
